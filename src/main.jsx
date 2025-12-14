77cedc345a11322a9b1d384205b5faf592bbd993
Najmi Zulhusni <najmizulhusninajmisapuan02@gmail.com>
2025-12-15 00:17:24 +0800

    Add Vercel Speed Insights for performance monitoring

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import App from './App.jsx'
 import FAQ from './FAQ.jsx'
 import Admin from './Admin.jsx'
@@ -14,6 +15,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <Route path="/faq" element={<FAQ />} />
         <Route path="/admin" element={<Admin />} />
       </Routes>
+      <SpeedInsights />
     </Router>
   </React.StrictMode>,
 )
